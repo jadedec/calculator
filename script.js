6f29f3727388a91f75586ea9b20e59bf7b92e88f
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-01 14:42:33 +0100

    number 1 got

--- script.js
+++ script.js
@@ -1,13 +1,111 @@
-// backspace button
-//if display.length >0, delete it one by one
 
-const backspace = () => {
-    displayStr = document.querySelector("calculatorDisplay_output");
-    for (let i = 0; i < displayStr.length; i++) {
-        document.querySelector("calculatorDisplay_output") = displayStr.substring(0, displayStr.length - 1);
-        if (displayStr == "") {
-            document.querySelector("calculatorDisplay_output") = "";
-        }
+//number button
+//button pressed,log in the console,
+//shown in the display
+//querySelectorAll .class
+//store the first number before click on operate button
+//store the operator
+//start the second number
+//click =  store  second number
+//calculate get result shown in display
+
+//global variable
+let num1 = "";
+let num2 = "";
+let displayString = "";
+
+
+const displayBox = document.querySelector("#display");
+
+const numberButton = document.querySelectorAll(".calculatorKeyboard_button--number");
+const operateButton = document.querySelectorAll(".calculatorKeyboard_button--operate");
+
+
+
+const buttonClick = (event) => {
+    //button clicked
+    console.log("Button Clicked");
+    // console.log(event.target.innerHTML);
+
+    //connect inputs together
+    displayString += event.target.innerHTML;
+    console.log(displayString);
+
+    displayBox.innerHTML = displayString;
+}
+//make it shown in display
+//number and operator displayed
+numberButton.forEach(button => { button.addEventListener("click", buttonClick) });
+operateButton.forEach(button => { button.addEventListener("click", buttonClick) });
+
+//when operator clicked, give the number to num1
+const operatorClick = (event) => {
+    console.log("Operator clicked");
+    num1 = parseFloat(displayBox.innerHTML);
+    console.log(`number1 is ${num1}`);
+}
+operateButton.forEach(button => { button.addEventListener("click", operatorClick) });
+
+//when equal clicked, give the number to num2
+const equalClick = (event) => {
+    console.log("Equal clicked");
+    // num2 =;
+    console.log(`number2 is ${num2}`);
+    calculation()
+}
+
+
+
+
+
+
+
+
+//AC button clear function
+const ACButton = document.querySelector("#AC");
+const clearResult = (event) => {
+    document.querySelector("#display").value = "";
+}
+ACButton.addEventListener("click", clearResult);
+
+
+// backspace function for  button
+const backspaceButton = document.querySelector("#delete");
+const backspace = (event) => {
+    const displayStr = document.querySelector("#display").value;
+    const newDisplay = displayStr.substring(0, displayStr.length - 1);
+    document.querySelector("#display").value = newDisplay;
+}
+backspaceButton.addEventListener("click", backspace);
+
+//+/-
+const switchButton = document.querySelector("#symbol");
+const switchSymbol = (event) => {
+    document.querySelector("#display").value = 0 - displayStr;
+}
+switchButton.addEventListener("click", switchSymbol);
+
+
+
+
+//+-×÷ switch cases
+const calculation = () => {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '×':
+            return num1 * num2;
+        case '÷':
+            return num1 / num2;
     }
 }
 
+//!
+function factorial(n) {
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return (n * factorial(n - 1));
+}
